274221f05859a6c4dbb7da481f656619b2dcb3a6
Nicholas <nicholas@ulmon.com>
2018-08-02 12:12:32 +0200

    Created action creator and async action to handle tweet liked

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,35 @@
+import { saveToggleTweet } from '../utils/api'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
 
 export function receiveTweets(tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets
+  }
+}
+
+function toggleTweet ({id, authedUser, isLiked}) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    isLiked
+  }
+}
+
+export function handleToggleTweet (info) {
+  return (dispatch) => {
+    dispatch(toggleTweet(info))
+
+    return saveToggleTweet(info)
+      .catch((e) => {
+        console.warn('Error in handleToggleTweet: ', e)
+        dispatch(toggleTweet(info))
+        alert('There was an error liking the tweet. Try again')
+      })
+
+
   }
 }

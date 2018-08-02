d92aa198cc9f939a8bd9c419bda0b3f2f6eb8228
Nicholas <nicholas@ulmon.com>
2018-08-02 12:20:59 +0200

    Updated tweets reducer to handle tweet liked toggle

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS } from '../actions/tweets'
+import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets'
 
 export default function tweets (state = {}, action) {
   switch (action.type) {
@@ -6,6 +6,16 @@ export default function tweets (state = {}, action) {
       return {
         ...state,
         ...action.tweets
+      }
+    case TOGGLE_TWEET :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          liked: action.hasLiked === true
+            ? state[action.id].liked.filer((uid) => uid !== action.authedUser )
+            : state[action.id].liked.concat([action.authedUser])
+        }
       }
     default:
       return state
